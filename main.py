c3fa1edaa11fdd2518d0154fe45299f079730adf
Helton Barbosa <heltonbs@gmail.com>
2023-12-31 08:22:10 -0300

    Refatoração

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-valor_conta = float(input('Qual o valor da conta? '))
 DESCONTO_10 = 10
 DESCONTO_20 = 20
 DESCONTO_30 = 30
 DESCONTO_50 = 50
 
-if valor_conta <= 1000:
+valor_conta = float(input('Qual o valor da conta? '))
+
+valor_10 = valor_conta <= 1000
+valor_20 = valor_conta > 1000 and valor_conta <= 5000
+valor_30 = valor_conta > 5000 and valor_conta <= 10000
+
+if valor_10:
     valor_total = valor_conta - (valor_conta * (DESCONTO_10 / 100))
     print(f'Valor total com desconto de 10% a ser pago: {valor_total:.2f}')
-elif valor_conta > 1000 and valor_conta <= 5000:
+elif valor_20:
     valor_total = valor_conta - (valor_conta * (DESCONTO_20 / 100))
     print(f'Valor total com desconto de 20% a ser pago: {valor_total:.2f}')
-elif valor_conta > 5000 and valor_conta <= 10000:
+elif valor_30:
     valor_total = valor_conta - (valor_conta * (DESCONTO_30 / 100))
     print(f'Valor total com desconto de 30% a ser pago: {valor_total:.2f}')
 else:
